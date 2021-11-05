19ffba6afde7007002e8f34a9dd4cc791f1b7b25
masum8080 <86814280+masum8080@users.noreply.github.com>
2021-11-05 14:00:39 +0600

    Update dictionary.php

--- dictionary.php
+++ dictionary.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" href="css/css/css/all.min.css">
+		<link rel="stylesheet" href="css/all.min.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
